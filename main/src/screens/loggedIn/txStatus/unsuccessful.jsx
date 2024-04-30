9241a782a011e6648571aef1ec5e16d02bcc72fb
Jashan Shetty <43118727+eth-jashan@users.noreply.github.com>
2024-05-01 00:41:37 +0530

    Revert "Merge branch 'main' of https://github.com/xade-finance/mobile"
    This reverts commit a7988c9a1411fe0f0aa1ead1668f1834bc2f5b85, reversing
    changes made to c33c2bfe01abd70e38694023a6c72dd5325b8eb5.

--- main/src/screens/loggedIn/txStatus/unsuccessful.jsx
+++ main/src/screens/loggedIn/txStatus/unsuccessful.jsx
@@ -14,36 +14,60 @@ import {
 } from 'react-native';
 import {Text} from '@rneui/themed';
 import Video from 'react-native-video';
-const successVideo = require('./failed.mp4');
+const successVideo = require('./unsuccess.mov');
 
 export default function Component({navigation, route}) {
   error = route.params.error;
+  // Some routing must be done after Pending state
   console.log('Err:', error);
   return (
-    <View style={{width: '100%', height: '100%', backgroundColor: '#000'}}>
-      <View style={{ width: 400, height: 400, alignItems: 'center', justifyContent: 'center',marginTop:'30%' }}>
+    <View style={{width: '100%', height: '100%', backgroundColor: '#0C0C0C'}}>
+      <Text
+        style={{
+          color: '#fff',
+          fontSize: 30,
+          marginTop: '20%',
+          textAlign: 'center',
+          fontFamily: `EuclidCircularA-Bold`,
+        }}>
+        Transaction Unsuccessful
+      </Text>
+      <View
+        style={{
+          width: '80%',
+          marginTop: '20%',
+          marginLeft: Platform.OS == 'ios' ? '12%' : '22%',
+        }}>
         <Video
           source={successVideo}
-          style={{ width: 600, height: 600 }}
-          resizeMode={'cover'}
+          style={{width: 300, height: 300}}
           controls={false}
           repeat={true}
-          muted={true}
           ref={ref => {
             this.player = ref;
           }}
         />
       </View>
-      <TouchableOpacity onPress={() => navigation.push('Settings')}>
+      <Text
+        style={{
+          color: '#fff',
+          fontSize: 20,
+          marginTop: '20%',
+          textAlign: 'center',
+          fontFamily: `EuclidCircularA-Medium`,
+        }}>
+        {JSON.stringify(error)}
+      </Text>
+      <TouchableOpacity onPress={() => navigation.push('Payments')}>
         <Text
           style={{
             color: '#fff',
             fontSize: 22,
             marginTop: '10%',
             textAlign: 'center',
-            fontFamily: `Unbounded-Medium`,
+            fontFamily: `EuclidCircularA-Medium`,
           }}>
-          GO BACK
+          Return Home
         </Text>
       </TouchableOpacity>
     </View>
